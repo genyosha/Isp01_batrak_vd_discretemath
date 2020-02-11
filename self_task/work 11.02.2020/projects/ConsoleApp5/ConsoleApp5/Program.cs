6516a37af342f4994950f1e3839a2cd1079a7826
Vladimir <volodia_mail1410@mail.ru>
2020-02-11 22:52:20 +0400

    in self_task\work 11.02.2020 add projects\ConsoleApp5

---
+++ self_task/work 11.02.2020/projects/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {4, 2 , 5, 9, 333};
+            //проверка метода Bubble
+            SortAray.Bubble(array);
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+            //проверка метода Selection
+            SortAray.Selection(array);
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+            Console.ReadKey();
+        }
+    }
+}
